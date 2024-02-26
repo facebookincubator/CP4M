236f54a2c891e7212b58a536ec34c39215eb9cbb
Utkarsh Singh <66885923+utkarshsingh77@users.noreply.github.com>
2024-02-26 17:54:10 -0500

    change baseurl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://facebookincubator.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/CP4M/',
+  baseUrl: '/CP4M/docs/build/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
